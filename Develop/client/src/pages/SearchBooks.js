1ca75de2ab179a24d17cefbc24a007b36aee606c
Jess Hause Ugray <jess.ugray@gmail.com>
2022-01-20 19:08:59 -0500

    client side updates

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -22,7 +22,7 @@ const SearchBooks = () => {
 
 
  const [saveBook] = useMutation(SAVE_BOOK);
-console.log("SEARCH BOOK")
+  console.log("SEARCH BOOK")
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
